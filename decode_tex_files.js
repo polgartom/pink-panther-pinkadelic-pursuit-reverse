463bb79710ad2bf400cbeb7585b3fb08752554d5
polgar <polgarey@gmail.com>
2024-06-20 22:47:54 +0200

    Decode tex files

--- decode_tex_files.js
+++ decode_tex_files.js
@@ -1,7 +1,7 @@
 const ll = console.log;
 
-const HEADER_SIZE = 12;
-let BYTES_PER_PIXEL = 2;
+const HEADER_SIZE = 12;   // in bytes
+let BYTES_PER_PIXEL = 2;  // BPP
 let CHUNK_END_OFFSET = 0;
 
 let file = null; // from url query param ?tex=
@@ -9,74 +9,6 @@ let pixels = [];
 let palette = {};
 let data = '';
 let bytes = [];
-
-Array.prototype.get8Int = function() {
-    let b = this.splice(0, 1);
-    return (b[0] & 0xFF);
-}
-
-Array.prototype.getL16Int = function() { // Little endian
-    let b = this.splice(0, 2);
-    return ( (b[1] << 8) | b[0] ) & 0xFF_FF;
-}
-
-Array.prototype.getL24Int = function() { // Little endian
-    let b = this.splice(0, 3);
-    return ( (b[2] << 16) | (b[1] << 8) | b[0] ) & 0xFF_FF_FF;
-}
-
-Array.prototype.getL32Int = function() { // Little endian
-    let b = this.splice(0, 4);
-    return ( (b[3] << 24) | (b[2] << 16) | (b[1] << 8) | b[0] ) & 0xFF_FF_FF_FF;
-}
-
-function decode_16bit_rgb(rgb565) {
-    // @Todo: Not 100% right
-    blue = (rgb565 >> 10) & 0b11111;
-    green = (rgb565 >> 4) & 0b111111;
-    red = rgb565 & 0b11111;
-
-    blue = Math.round(blue / 31 * 255);
-    green = Math.round(green / 63 * 255);
-    red = Math.round(red / 31 * 255);
-
-    return [red, green, blue];
-}
-
-
-function preload() {
-    file = new URLSearchParams(window.location.search).get('tex');
-    if (!file) {
-        window.alert('PLEASE SELECT A TEX FILE!');
-        return;
-    }
-
-    tex = loadBytes(file);
-}
-
-function unpack_16(n) {
-    return [
-        (n >> 8 ) & 0xFF,
-        (n >> 0 ) & 0xFF,
-    ];
-}
-
-function unpack_24(n) {
-    return [
-        (n >> 16 ) & 0xFF,
-        (n >> 8  ) & 0xFF,
-        (n >> 0  ) & 0xFF,
-    ];
-}
-
-function unpack_32(n) {
-    return [
-        (n >> 24) & 0xFF,
-        (n >> 16) & 0xFF,
-        (n >> 8 ) & 0xFF,
-        (n >> 0 ) & 0xFF,
-    ];
-}
 
 function setup() {
     bytes = [...tex.bytes];
@@ -102,7 +34,7 @@ function setup() {
     if (PALETTE_MODE) {
         // In palette mode, we need to construct the palette by calculating its offset.
         // The palette address will be after the main chunk, where each byte represents the relative offset on the palette.
-        let chunk = bytes.splice(0, CHUNK_END_OFFSET-HEADER_SIZE);
+        let chunk = bytes.splice(0, CHUNK_END_OFFSET - HEADER_SIZE);
         while (chunk.length) {
             let color_index = chunk.get8Int();
             if (palette[color_index] === undefined) {
@@ -146,4 +78,71 @@ function draw() {
     }
 
     updatePixels();
+}
+
+function preload() {
+    file = new URLSearchParams(window.location.search).get('tex');
+    if (!file) {
+        window.alert('PLEASE SELECT A TEX FILE!');
+        return;
+    }
+
+    tex = loadBytes(file);
+}
+
+function decode_16bit_rgb(rgb565) {
+    // @Todo: Not 100% right
+    blue = (rgb565 >> 10) & 0b11111;
+    green = (rgb565 >> 4) & 0b111111;
+    red = rgb565 & 0b11111;
+
+    blue = Math.round(blue / 31 * 255);
+    green = Math.round(green / 63 * 255);
+    red = Math.round(red / 31 * 255);
+
+    return [red, green, blue];
+}
+
+Array.prototype.get8Int = function() {
+    let b = this.splice(0, 1);
+    return (b[0] & 0xFF);
+}
+
+Array.prototype.getL16Int = function() { // Little endian
+    let b = this.splice(0, 2);
+    return ( (b[1] << 8) | b[0] ) & 0xFF_FF;
+}
+
+Array.prototype.getL24Int = function() { // Little endian
+    let b = this.splice(0, 3);
+    return ( (b[2] << 16) | (b[1] << 8) | b[0] ) & 0xFF_FF_FF;
+}
+
+Array.prototype.getL32Int = function() { // Little endian
+    let b = this.splice(0, 4);
+    return ( (b[3] << 24) | (b[2] << 16) | (b[1] << 8) | b[0] ) & 0xFF_FF_FF_FF;
+}
+
+function unpack_16(n) {
+    return [
+        (n >> 8 ) & 0xFF,
+        (n >> 0 ) & 0xFF,
+    ];
+}
+
+function unpack_24(n) {
+    return [
+        (n >> 16 ) & 0xFF,
+        (n >> 8  ) & 0xFF,
+        (n >> 0  ) & 0xFF,
+    ];
+}
+
+function unpack_32(n) {
+    return [
+        (n >> 24) & 0xFF,
+        (n >> 16) & 0xFF,
+        (n >> 8 ) & 0xFF,
+        (n >> 0 ) & 0xFF,
+    ];
 }
